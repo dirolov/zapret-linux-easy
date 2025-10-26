f8ab1e710466920274f15939d11eda39e955ed35
Vanya Dirolov <dgeefee768@gmail.com>
2025-10-26 11:35:51 +0300

    я попутал директории

--- install.sh
+++ install.sh
@@ -55,10 +55,10 @@ case $choice in
 esac
 
 if command -v systemctl >/dev/null 2>&1 && [ -d /run/systemd ]; then
-bash $PWD/scripts/systemd.sh
+bash $PWD/files/scripts/systemd.sh
 
 elif command -v openrc-run >/dev/null 2>&1 || [ -d /run/openrc ]; then
-bash $PWD/scripts/openrc.sh
+bash $PWD/files/scripts/openrc.sh
     
 else
     echo "Не удалось определить систему инициализации (systemd или OpenRC не найдены)."
