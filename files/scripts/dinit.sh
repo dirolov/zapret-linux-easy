d4f31d036c1bff2f461f57e2687c9a0eea92cf0a
Vanya Dirolov <dgeefee768@gmail.com>
2025-10-27 15:00:42 +0300

    test dinit

---
+++ files/scripts/dinit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cat <<EOF > /etc/dinit.d/zapret
+type 	   = process
+restart	   = true
+depends-on = network.target
+logfile    = /var/log/dinit/zapret.log
+command    = /opt/zapret/starter.sh
+EOF
+    
+    dinitctl start zapret
+    dinitctl enable zapret
+    echo "Установка завершена. zapret теперь в папке /opt/zapret, папку в Загрузках можно удалить."
